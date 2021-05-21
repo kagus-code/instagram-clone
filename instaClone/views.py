fac672a13457e97d33c42a446082b500b10cd47c
Eston Kagwima <ekagwima745@gmail.com>
2021-05-22 02:07:27 +0300

    adding templates folder and configuring settings

--- instaClone/views.py
+++ instaClone/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.core.checks import messages
 from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+import datetime as dt
 
 # Create your views here.
 
 def landing (request):
-  return render ()
+
+  title = 'instagram'
+  return render (request,'index.html',{'title':title,})
