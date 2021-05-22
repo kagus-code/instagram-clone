7c89914c1e053c8ceec0239fd587b04406ac3715
Eston Kagwima <ekagwima745@gmail.com>
2021-05-22 17:45:29 +0300

    adding profile model and its methods

--- instaClone/models.py
+++ instaClone/models.py
@@ -2,3 +2,33 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user_name = models.CharField(max_length=30)
+    profile_pic = CloudinaryField('image',null=True)
+    bio = models.TextField(max_length=500, default="Bio", blank=True)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10, blank=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_username(self, user_name):
+        self.objects.filter(user_name=user_name).delete()
+
+    @classmethod
+    def update_username(cls,user_name,new_name):
+        update = Profile.objects.filter(user_name=user_name).update(user_name=new_name)
+        return update
+
+    @classmethod
+    def update_bio(cls,user_name,bio):
+        update = Profile.objects.filter(user_name=user_name).update(bio=bio)
+        return update
+        
+    def __str__(self):
+        return self.user_name
+
+    class Meta:
+        ordering = ['user_name']
