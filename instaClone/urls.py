38670433b41ace90b2a558e7696b167f762d210d
Eston Kagwima <ekagwima745@gmail.com>
2021-05-22 03:31:39 +0300

    adding instagram favicon

--- instaClone/urls.py
+++ instaClone/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, re_path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 
 
 
@@ -9,6 +12,13 @@ from . import views
 urlpatterns = [
 
   re_path(r'^$', views.landing,name='landingPage'),
+
+
+
+
+
+
+  path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
 
 
 
