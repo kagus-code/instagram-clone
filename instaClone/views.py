b9ee62b0771a7db8916bad8b364e002f505c5a1b
Eston Kagwima <ekagwima745@gmail.com>
2021-05-23 12:53:22 +0300

    displaying image post on homepage

--- instaClone/views.py
+++ instaClone/views.py
@@ -2,10 +2,14 @@ from django.core.checks import messages
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
+from .models import Image,Profile,Comment
 
 # Create your views here.
 
 def landing (request):
+  post = Image.objects.all()
+  comments =Comment.objects.all()
+
 
   title = 'Instagram'
-  return render (request,'index.html',{'title':title,})
+  return render (request,'index.html',{'title':title,'Posts':post, 'comments':comments})
