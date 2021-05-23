c578adfa64e3834900bf813655ae028681184dc4
Eston Kagwima <ekagwima745@gmail.com>
2021-05-23 11:57:23 +0300

    adding comment model to admin.py

--- instaClone/admin.py
+++ instaClone/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Image,Profile
+from .models import Image,Profile,Comment
 
 admin.site.register(Image)
 admin.site.register(Profile)
+admin.site.register(Comment)
 
-# Register your models here.
